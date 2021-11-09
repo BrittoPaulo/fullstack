ae138665b1790585616342ce39d03a24264f9641
brittopaulo <brittopaulo.dev@gmail.com>
2021-11-09 00:37:38 -0300

    fix(API): :bug: Associação class Bd errada

--- api/src/main/factories/usecases/deletar-pessoa.factory.ts
+++ api/src/main/factories/usecases/deletar-pessoa.factory.ts
@@ -1,12 +1,12 @@
-import { BdAdicionarPessoa } from '@/data/usecases'
+import { BdDeletarPessoa } from '@/data/usecases'
 import { PessoaRepository } from '@/infra/orm/repositories'
-import { AdicionarPessoa } from '@/domain/usecases'
+import { DeletarPessoa } from '@/domain/usecases'
 import { DELETAR_PESSOA_FACTORY } from '@/main/factories/providers'
 
 export const deletarPessoaFactory = {
   provide: DELETAR_PESSOA_FACTORY,
-  useFactory: (pessoaRepository: PessoaRepository): AdicionarPessoa => {
-    return new BdAdicionarPessoa(pessoaRepository)
+  useFactory: (pessoaRepository: PessoaRepository): DeletarPessoa => {
+    return new BdDeletarPessoa(pessoaRepository)
   },
   inject: [PessoaRepository],
 }
